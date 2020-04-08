5f7d8a9bd0392ea9f2181da7a2cf3ff848684d98
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-08 10:09:07 +0700

    #4 - Add RekapLive DTO

---
+++ src/main/java/com/hendisantika/datetest/RekapLiveDto.java
@@ -0,0 +1,46 @@
+package com.hendisantika.datetest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : date-test
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/04/20
+ * Time: 09.13
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RekapLiveDto {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(length = 40)
+    private String id;
+
+    @Temporal(TemporalType.DATE)
+    private Date tanggal;
+
+    private String kode;
+
+    private int terisi;
+
+    private int sisa;
+
+    @Temporal(TemporalType.TIME)
+    private Date catat;
+
+}
